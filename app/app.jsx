25a105a9be41d9d48b10103e2992abb86bd58e93
KangboLu <redstonegranola@gmail.com>
2017-07-03 08:30:44 +0800

    Add source code for boilerplate

---
+++ app/app.jsx
@@ -0,0 +1,9 @@
+// import necessary modules
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+// render component on the DOM
+ReactDOM.render(
+    <h1>Boilerplate app</h1>,
+    document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    entry: './app/app.jsx',
+    output: {
+        path: __dirname,
+        filename: './public/bundle.js'
+    },
+    resolve: {
+        root: __dirname,
+        alias: {
+        },
+        extensions: ['', '.js', '.jsx']
+    },
+    // using babel-loader to compile code to react and es2015
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015', 'stage-0']
+                },
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/
+            }
+        ]
+    }
+}
